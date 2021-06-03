c4f253e5e4b56e977901c610c2fa3b97a9a25615
Sebastian Martinez <smariver@gmail.com>
2021-06-03 20:38:48 -0300

    First 2 exercises Repeat string Reverse string

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num<0) return "ERROR";
+    let returnedString = "";
+    for(i=0; i<num; i++){
+        returnedString += string;
+    }
+    return returnedString;
+    
 };
 
 module.exports = repeatString;
