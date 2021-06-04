c4bd29d61921f744a09dc184cc1ee68f4c6ceb5b
Sebastian Martinez <smariver@gmail.com>
2021-06-03 23:11:40 -0300

    Modified sumAll.js Checks that values are numbers and not negative Sums them all up

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0, aux;
 
+    if(typeof num1 !== "number" || typeof num2 !== "number" || num1 < 0 || num2 < 0)
+        return "ERROR";
+    
+    if(num1>num2){
+        aux = num1;
+        num1 = num2;
+        num2 = aux;
+    }
+    
+    for(let i = num1; i < num2+1; i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 module.exports = sumAll;
