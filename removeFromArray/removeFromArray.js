d6f255d4b5c652441010db2d3a8c50f1a95e72b8
Sebastian Martinez <smariver@gmail.com>
2021-06-03 22:18:32 -0300

    Modified removedFromArray Function grabs arguments, checks if the first argument is actually an array, if not makes it so. All the other arguments are sent to the loop and added to the new array badArray. Creates two variables and assigns the lengths of the arrays to them. Goes through both arrys in two connected for loops, if theres a match, it splices that element and substracts one i since that element got removed and a new one takes its place.

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(goodArray) {
+    let i, j;
+    if(Array.isArray(goodArray) === false)
+        goodArray = [goodArray];
+  
+    let badArray = new Array(arguments.length);
+    for(i = 0; i < badArray.length; i++){
+        badArray[i] = arguments[i];
+    }
+    
+    console.log(badArray);
+    let arrayLength = goodArray.length;
+    let arrayLengthBad = badArray.length;
 
+    for(i=0; i < arrayLength; i++){
+        for(j=0; j<arrayLengthBad; j++){
+            if(goodArray[i]===badArray[j]){
+                goodArray.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return goodArray;
 };
 
 module.exports = removeFromArray;
