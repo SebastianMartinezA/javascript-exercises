1e6a54c8d6eb7ae193ab4f2aa2df4757606acebb
Sebastian Martinez <smariver@gmail.com>
2021-06-19 22:35:42 -0300

    Palindromes resolved

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
-};
+const palindromes = function (daString) {
+    return(daString.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ").toLowerCase().replace(/\s/g,'') === 
+    daString.split("").reverse().join("").replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ").toLowerCase().replace(/\s/g,''));
+}
 
 module.exports = palindromes;
